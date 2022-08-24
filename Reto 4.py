fafc3de7afdebbf28f8b9f6c8485609abdaa1dcd
Mdiaz06 <110851594+Mdiaz06@users.noreply.github.com>
2022-08-24 13:24:43 -0500

    Add files via upload

---
+++ Reto 4.py
@@ -0,0 +1,70 @@
+import datetime
+from datetime import date, time, datetime
+class Product: 
+    def __init__(self, id, name, price):
+        self.id= id
+        self.name= name
+        self.price= float
+        self.price_history= dict {date:price}
+
+class User:
+    def __int__(self, id, user_name):
+        self.id = id
+        self.user_name = user_name
+        self.balance = 0
+        self.order_list= []
+car = []   
+user_one = User(123,"Catalina Monsalve")
+print(f"Bienvenido {user_one.user_name}, su saldo actual es: {user_one.amount}")
+
+    def add_product_to_car(self, prod: Product):
+        p1 = Product(101, "Libro los gatos de ulthar", 50.000)
+        p2 = Product(102,"Sandalias", 35.000)
+        p3 = Product(103,"Cuaderno 100 hojas", 6.500) 
+        p4 = Product(104,"Libro Clean code", 20.000)
+        p5 = Product(105,"Bolso cuatro piezas",102.000)
+
+        car.append(p1)
+        car.append(p2)
+        car.append(p3)
+        car.append(p4)
+        car.append(p5)
+        i=0
+        while i<len(car):
+            print(car[i].id," Product: ",car[i].name, "- Price:", car[i].price,"$")
+            i+=1
+    add_product_to_car()
+
+    def consolidate_order(self, order_id: int):
+        a = a
+    
+    def add_to_balance(self, amount:float):
+        self.amount = 90.000
+        return self.amount
+    add_to_balance()
+    
+    def plot_order_history():
+
+class Order:
+    def __init__(self,id):
+        self.id= id
+        self.product_list= []
+        self.date= date
+        self.total= float
+        self.status= bool
+
+
+
+
+p1 = Product(101, "Libro los gatos de ulthar", 50.000)
+p2 = Product(102,"Sandalias", 35.000)
+p3 = Product(103,"Cuaderno 100 hojas", 6.500) 
+p4 = Product(104,"Libro Clean code", 20.000)
+p5 = Product(105,"Bolso cuatro piezas",102.000)
+
+
+
+
+
+
+        
